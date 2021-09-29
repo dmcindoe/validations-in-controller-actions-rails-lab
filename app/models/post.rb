58da35b46aee17afa8ccaaf259fa74b2951b7d68
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-29 11:22:46 -0400

    commit tree 19e5135a26630f31cdb79e5a29458bc177a6ad06

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true, uniqueness: true
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validates :content, length: {minimum: 100}
+
+
+
 end
